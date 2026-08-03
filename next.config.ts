88a8c56d4e99041d527035e0098c980dd8f25bb5
Meharab-27 <meharabrahman100115@gmail.com>
2026-08-03 21:23:57 +0600

    second commit

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,11 @@ const nextConfig: NextConfig = {
 <<<<<<< HEAD
 =======
  
+<<<<<<< HEAD
   output: 'export',
 >>>>>>> ee52587 (chore: initialize Next.js project structure and configuration)
+=======
+>>>>>>> 44dda79 (docs: add API integration guide, environment configuration, and README documentation)
   cacheComponents: true,
   images: {
     remotePatterns: [
